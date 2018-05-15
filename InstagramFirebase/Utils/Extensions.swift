45e1a2efa68eb5e7426547092ffb7179dfa3c02b
Umuralp Kaytaz <ukaytaz@ku.edu.tr>
2018-05-15 16:15:28 +0300

    Create anchor extensions

---
+++ InstagramFirebase/Utils/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Umuralp Kaytaz on 14/05/2018.
+//  Copyright © 2018 Umuralp Kaytaz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) ->  UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
