6233bd46d9712b11d105c1cb57609452234607ac
Umuralp Kaytaz <ukaytaz@ku.edu.tr>
2018-05-21 21:34:48 +0300

    Fetch photos framework

---
+++ InstagramFirebase/AddContent/PhotoSelectorCell.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoSelectorCell.swift
+//  InstagramFirebase
+//
+//  Created by Umuralp Kaytaz on 21/05/2018.
+//  Copyright © 2018 Umuralp Kaytaz. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+        
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
