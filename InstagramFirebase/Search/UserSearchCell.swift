a10b46f3f2bf23ce1dfb7fc98eefe6934ec53c29
Umuralp Kaytaz <ukaytaz@ku.edu.tr>
2018-05-24 13:14:15 +0300

    user search controller

---
+++ InstagramFirebase/Search/UserSearchCell.swift
@@ -0,0 +1,59 @@
+//
+//  UserSearchCell.swift
+//  InstagramFirebase
+//
+//  Created by Umuralp Kaytaz on 24/05/2018.
+//  Copyright © 2018 Umuralp Kaytaz. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchCell: UICollectionViewCell {
+    
+    let profileImageView: CustomImageView = {
+        let iv = CustomImageView()
+        iv.backgroundColor = .red
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    let usernameLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "Username"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+        
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+                
+        addSubview(profileImageView)
+        addSubview(usernameLabel)
+        
+        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        profileImageView.layer.cornerRadius = 50/2
+        profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        usernameLabel.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        let seperatorView = UIView()
+        seperatorView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        addSubview(seperatorView)
+        seperatorView.anchor(top: nil, left: usernameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+
+
+
+
